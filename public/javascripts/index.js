63a11942823795d409975b87104c174f5f4a1221
younsoolim <ylim27@my.bcit.ca>
2021-10-01 21:02:41 -0700

    jquery chagnes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,43 @@
 // blank
+$(document).ready(function(){
+  
+  hide = $(".hide");
+  correct = $(".true");
+  wrong = $(".false")
+  answerBox = $("#answer-text")
+  leftArrow = $(".left-arrow")
+  rightArrow = $(".right-arrow")
+
+  showAnswer()
+
+})
+
+
+// ============ QUIZ 1 ============
+
+function showAnswer()
+{ 
+  correct.on("click", function()
+  {
+    answerBox.removeClass("hide")
+  })  
+
+  wrong.on("click", function()
+  {
+    answerBox.addClass("hide")
+  })
+}
+
+function goToPrevious()
+{
+  leftArrow.on("click", function()
+  {
+    
+  })
+
+}
+
+function goToNext()
+{
+
+}
